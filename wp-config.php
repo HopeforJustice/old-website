f10afe38f1585a7c874a0af070d34bd05a9d7e7c
James Holt <james.holt@hopeforjustice.org>
2021-11-25 15:00:06 +0000

    test-sourcetree

--- wp-config.php
+++ wp-config.php
@@ -3,6 +3,7 @@
  * The base configurations of the WordPress.
  *
  *
+ *
  * This file has the following configurations: MySQL settings, Table Prefix,
  * Secret Keys, and ABSPATH. You can find more information by visiting
  * {@link https://codex.wordpress.org/Editing_wp-config.php Editing wp-config.php}
